04f1836247796670b3c2f69dfc8e0ef0dcf14f67
Lucianodev2000 <nano.lucianocardoso@gmail.com>
2024-11-21 05:34:04 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+let listaGenerica = [];
+let listaLiguagensDeProgramação = ['JavaScript', 'C','C++', 'Kotlin', 'Python'];
+listaLiguagensDeProgramação.push('Java', 'Ruby', 'GoLang');
+let lista3Nomes = ['Luciano', 'Talita', 'Monark'];
+console.log(lista3Nomes[0]);
+console.log(lista3Nomes[1]);
+console.log(lista3Nomes[2]);
